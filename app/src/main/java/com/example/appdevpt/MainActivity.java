31bdf460d24fdfa29c313d6ebaf05abd584f2773
Raphael Jay Escosia <escosiaraphael@gmail.com>
2024-05-26 13:54:52 +0800

    menu

--- app/src/main/java/com/example/appdevpt/MainActivity.java
+++ app/src/main/java/com/example/appdevpt/MainActivity.java
@@ -7,61 +7,38 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-//    public static final String URL = "jdbc:mysql://localhost:3306/dbmobdev";
-//
-//    public static final String USERNAME = "root";
-//    public static final String PASSWORD ="";
-//
-//    static Connection getConnection(){
-//        Connection c = null;
-//        try {
-//            Class.forName("com.mysql.cj.jdbc.Driver");
-//            c = DriverManager.getConnection(URL,USERNAME,PASSWORD);
-//            System.out.println("DB connected!");
-//        } catch (SQLException | ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        return c;
-//    }
-
     EditText username;
     EditText password;
     Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        getConnection();
         setContentView(R.layout.activity_main);
+
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
         loginButton = findViewById(R.id.loginButton);
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
                     Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-                    //Intent intent = new Intent(MainActivity.this, MainMenu.class);
-                   // startActivity(intent);
+                    Intent intent = new Intent(MainActivity.this, menu.class);
+                    startActivity(intent);
                 } else {
                     Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
                 }
             }
-
         });
-
-
     }
+
     public void goToSignUpPage(View view) {
         Intent intent = new Intent(this, RegisterPage.class);
-       startActivity(intent);
+        startActivity(intent);
     }
-
-
 }
