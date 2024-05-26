f71abc9aacc7f0a74f90b96b724656b4606cae59
Matt <mattcedricybanezzz@gmail.com>
2024-05-26 23:41:44 +0800

    Interesting Stuff

--- app/src/main/java/com/example/appdevpt/MainActivity.java
+++ app/src/main/java/com/example/appdevpt/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                if (username.getText().toString().equals("Matt Cedric") && password.getText().toString().equals("mattcedric")) {
                     Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(MainActivity.this, menu.class);
                     startActivity(intent);
